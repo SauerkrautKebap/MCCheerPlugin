dce4ae4f4322aed8520b513edb32671a9f9dc49d
David <DavidSpecht@protonmail.com>
2021-01-31 15:58:38 +0100

    First Commit

---
+++ src/com/sauerkrautkebap/mccheerplugin/Main.java
@@ -0,0 +1,15 @@
+package com.sauerkrautkebap.mccheerplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
